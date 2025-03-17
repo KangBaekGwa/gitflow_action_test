548321c07e48ea8054c265eca1c283eeda411484
KangBaekGwa <ksu9801@gmail.com>
2025-03-17 14:11:17 +0900

    first

---
+++ src/main/java/z9/actiontest/ActiontestApplication.java
@@ -0,0 +1,13 @@
+package z9.actiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiontestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiontestApplication.class, args);
+    }
+
+}
